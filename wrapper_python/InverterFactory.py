d95f3c3f2ad68bd3de09cd83899da5d0b8f836a6
Prafull Gaikwad <praf1249@gmai.com>
2017-04-13 15:03:20 -0700

    Resolved issues to run the factory method

--- wrapper_python/InverterFactory.py
+++ wrapper_python/InverterFactory.py
@@ -2,30 +2,30 @@ import json
 
 __author__ = 'Prafull'
 
-from inverter import Inverter
-from outback import OutbackInverter
+from outback import OutbackInverterClass
+from outback.__utils__ import  logger, json
 
 INV_TYPES = {
     # add the types of inverter
-    "outback": OutbackInverter,
+    "outback": OutbackInverterClass.OutbackInverterClass,
 }
 
 class InverterFactory(object):
     # Create based on class name:
+    @staticmethod
     def factory():
         """
-
         :return:
         """
         with open("inverter.json", 'r') as f:
             inv_type_json = json.load(f)
+            logger.info("inverter type string " + str(inv_type_json))
         if "type" not in inv_type_json:
-            assert 0, "Problem with inverter_type file: " + type
-        type = inv_type_json["type"]
+            assert 0, "Problem with inverter_type file: inverter.json"
+        type_inv = inv_type_json["type"]
 
-        if type not in INV_TYPES:
-            assert 0, "Bad inverter type creation: " + type
-        inv_instance = INV_TYPES.get(type)
+        if type_inv not in INV_TYPES:
+            assert 0, "Bad inverter type creation: " + type_inv
+        inv_instance = INV_TYPES[type_inv]
+        print type(inv_instance)
         return inv_instance(inv_type_json)
-
-    factory = staticmethod(factory)
